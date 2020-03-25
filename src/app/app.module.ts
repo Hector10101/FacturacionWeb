ae7058e93854e5e3d64aeaa5726660d0bbb9174c
Hector Sanchez <20186183@itla.edu.do>
2020-03-25 16:41:38 -0400

    Navegacion agregada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './Componentes/navegacion/navegacion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+
   ],
   imports: [
     BrowserModule,
