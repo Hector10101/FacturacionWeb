6c4ae2a1f1cbfc236902a1570922602df0cb928c
Erickps123 <erickkpsanchez@gmail.com>
2020-03-28 13:54:10 -0400

    Login Working!

--- src/app/Componentes/Login/login/login.component.ts
+++ src/app/Componentes/Login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
+import { Route } from '@angular/compiler/src/core';
+import { User } from 'src/app/Modelos/user.models';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public Usuarios: any = [];
 
-  constructor() { }
+  public ID : string;
+  public Nombre : string;
+  public User : string;
+  public Password : string;
+  public Area : string;
+  public NombreArea: string;
+
+  constructor(private APIServices: APIservicesService) { }
 
   ngOnInit(): void {
+    this.GetUser();
   }
+  GetUser(){
+    this.APIServices.GetUser().subscribe((data: {}) => {
+        this.Usuarios = data;
+        console.log(this.Usuarios);
+    })
+    }
+
+    LogIn(newUser:HTMLInputElement,newPassword:HTMLInputElement){
+      for(let usuario of this.Usuarios){
+        if((newUser.value == usuario.user) && (newPassword.value== usuario.password)){
+          this.ID = usuario.id;
+          this.Nombre = usuario.nombre;
+          this.User = usuario.user;
+          this.Password = usuario.password;
+          this.Area = usuario.area;
+          this.NombreArea = usuario.nombredelArea;
+          let u: User = {nombredelArea: this.NombreArea};        
+          this.APIServices.setUserLoggedIn(u);
+    
+          console.log(u);
+        }
+      }
+     
+    }
 
 }
--- src/app/Componentes/Usuarios/ver-usuarios/ver-usuarios.component.ts
+++ src/app/Componentes/Usuarios/ver-usuarios/ver-usuarios.component.ts
@@ -22,3 +22,4 @@ export class VerUsuariosComponent implements OnInit {
     })
     }
 }
+ 
