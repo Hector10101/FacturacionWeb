dd3d0d8cacb045eafba2e43cf65f8712df109ca5
Hector Sanchez <20186183@itla.edu.do>
2020-03-28 16:36:28 -0400

    APIServices Editar Producto de la Sucursal Funcionando

--- src/app/Componentes/UserSucursal/Productos/edi-productos/edi-productos.component.ts
+++ src/app/Componentes/UserSucursal/Productos/edi-productos/edi-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
 
 @Component({
   selector: 'app-edi-productos',
@@ -7,9 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdiProductosComponent implements OnInit {
 
-  constructor() { }
+
+  public Productos: any = [];
+  public ID : string;
+  public Nombre : string;
+  public Descricipcion : string;
+  public Cantidad : number;
+  public FechaCaducidad: string;
+  public PrecioUnitario: number;
+  public PrecioTotal: number;
+  public seleccionado: string;
+  
+  constructor(private APIServices: APIservicesService) { }
 
   ngOnInit(): void {
+    this.GetProducto();
+  }
+
+  CantidadTotal(newCantidad: HTMLInputElement, newPrecioUnitario: HTMLInputElement,){
+    this.Cantidad =  parseInt(newCantidad.value);
+    this.PrecioUnitario =  parseInt(newPrecioUnitario.value);
+    this.PrecioTotal = this.PrecioUnitario * this.Cantidad;
+  }
+
+  GetProducto(){
+    this.APIServices.GetProducto().subscribe((data: {}) => {
+        this.Productos = data;
+        console.log(this.Productos);
+    })
+  }
+
+  GetProductosSelected(){
+    for(let producto of this.Productos){
+      if(this.seleccionado == producto.id){
+        this.ID = producto.id;
+        this.Nombre = producto.nombre;
+        this.Descricipcion = producto.descripción;
+        this.Cantidad = producto.cantidad;
+        this.FechaCaducidad = producto.fecha_caducidad;
+        this.PrecioUnitario = producto.precioUnitario;
+        this.PrecioTotal = producto.precioTotal;
+     
+      }
+    } 
+  }
+
+  UpdateProducto(newNombre: HTMLInputElement,newDescripcion: HTMLInputElement,newCantidad: HTMLInputElement,newFechaCaducidad: HTMLInputElement, newPrecioUnitario: HTMLInputElement, newPrecioTotal: HTMLInputElement) {
+    if(this.seleccionado != null){
+      if(this.seleccionado == this.ID){
+        if (window.confirm('Are you sure, you want to modify?')){
+          const Producto = {
+            Id: this.seleccionado, 
+            Nombre: newNombre.value,
+            Descripción: newDescripcion.value,
+            Cantidad: newCantidad.value,
+            Fecha_caducidad: newFechaCaducidad.value,
+            PrecioUnitario: newPrecioUnitario.value,
+            PrecioTotal: newPrecioTotal.value,
+        
+          };
+          this.APIServices.UpdateProducto(this.seleccionado, Producto).subscribe(data => {
+            this.GetProducto();
+            newNombre.value = '';
+            newDescripcion.value = '';
+            newCantidad.value = '';
+            newFechaCaducidad.value = '';
+            newPrecioUnitario.value = '';
+            newPrecioTotal.value = '';
+            this.seleccionado= '';
+          
+          })
+        }
+      }
+    }  
   }
 
 }
