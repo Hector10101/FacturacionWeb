afde5a4edf7a132081b79b8e79f3983a5528acc7
Hector Sanchez <20186183@itla.edu.do>
2020-03-28 16:28:30 -0400

    APIServices Eliminar Producto de la Sucursal Funcionando

--- src/app/Componentes/UserSucursal/Productos/eli-productos/eli-productos.component.ts
+++ src/app/Componentes/UserSucursal/Productos/eli-productos/eli-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
 
 @Component({
   selector: 'app-eli-productos',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EliProductosComponent implements OnInit {
 
-  constructor() { }
-
+  public Productos: any = [];
+  public ID : string;
+  public Nombre : string;
+  public Descricipcion : string;
+  public Cantidad : string;
+  public FechaCaducidad: string;
+  public PrecioUnitario: string;
+  public PrecioTotal: string;
+  public seleccionado: string;
+  
+  constructor(private APIServices: APIservicesService) { }
   ngOnInit(): void {
+    this.GetProducto();
   }
+  GetProducto(){
+    this.APIServices.GetProducto().subscribe((data: {}) => {
+        this.Productos = data;
+        console.log(this.Productos);
+    })
+    }
 
+    GetProductosSelected(){
+      for(let producto of this.Productos){
+        if(this.seleccionado == producto.id){
+          this.ID = producto.id;
+          this.Nombre = producto.nombre;
+          this.Descricipcion = producto.descripción;
+          this.Cantidad = producto.cantidad;
+          this.FechaCaducidad = producto.fecha_caducidad;
+          this.PrecioUnitario = producto.precioUnitario;
+          this.PrecioTotal = producto.precioTotal;
+       
+        }
+      } 
+    }
+    DeleteProducto() {
+      if(this.seleccionado != null){
+        if(this.seleccionado == this.ID){
+          if (window.confirm('Are you sure, you want to delete?')){
+            this.APIServices.DeleteProducto(this.seleccionado).subscribe(data => {
+              this.GetProducto();
+              this.ID = '';
+              this.Nombre = '';
+              this.Descricipcion = '';
+              this.Cantidad = '';
+              this.FechaCaducidad = '';
+              this.PrecioUnitario = '';
+              this.PrecioTotal = '';
+              this.seleccionado = '';
+            })
+          }
+        }
+      }  
+    }
 }
