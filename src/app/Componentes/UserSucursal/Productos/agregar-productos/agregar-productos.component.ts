256449eabfccb5977b332cb739873aaa5910b021
Hector Sanchez <20186183@itla.edu.do>
2020-03-28 16:18:41 -0400

    APIServices Agregar Producto de la Sucursal Funcionando

--- src/app/Componentes/UserSucursal/Productos/agregar-productos/agregar-productos.component.ts
+++ src/app/Componentes/UserSucursal/Productos/agregar-productos/agregar-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
 
 @Component({
   selector: 'app-agregar-productos',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarProductosComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private APIServices: APIservicesService) { }
+  public Productos: any = [];
+  precioTotal: number;
+  cantidad: number;
+  precioUnitario: number;
   ngOnInit(): void {
+  }
+  CantidadTotal(newCantidad: HTMLInputElement, newPrecioUnitario: HTMLInputElement,){
+    this.cantidad =  parseInt(newCantidad.value);
+    this.precioUnitario =  parseInt(newPrecioUnitario.value);
+    this.precioTotal = this.precioUnitario * this.cantidad;
+  }
+  PostProductos(newNombre: HTMLInputElement,newDescripcion: HTMLInputElement,newCantidad: HTMLInputElement,newFechaCaducidad: HTMLInputElement,newPrecioUnitario: HTMLInputElement, newPrecioTotal: HTMLInputElement) {
+    
+    const Producto = {
+    Nombre: newNombre.value,
+    Descripción: newDescripcion.value,
+    Cantidad: newCantidad.value,
+    Fecha_caducidad: newFechaCaducidad.value,
+    PrecioUnitario: newPrecioUnitario.value,
+    PrecioTotal: this.precioTotal,
+
+
+    };
+    this.APIServices.PostProducto(Producto).subscribe((registro: {}) => {
+      console.log(registro);
+      newNombre.value = '';
+      newDescripcion.value = '';
+      newCantidad.value = '';
+      newFechaCaducidad.value = '';
+      newPrecioUnitario.value = '';
+      newPrecioTotal.value = '';
+
+    })
   }
 
 }
