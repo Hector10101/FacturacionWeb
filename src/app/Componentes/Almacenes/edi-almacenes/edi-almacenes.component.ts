0230b07ada4c3fbf00ed20ff96632189d6175695
Erickps123 <erickkpsanchez@gmail.com>
2020-03-28 01:53:30 -0400

    APIServices

--- src/app/Componentes/Almacenes/edi-almacenes/edi-almacenes.component.ts
+++ src/app/Componentes/Almacenes/edi-almacenes/edi-almacenes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
 
 @Component({
   selector: 'app-edi-almacenes',
@@ -6,10 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edi-almacenes.component.scss']
 })
 export class EdiAlmacenesComponent implements OnInit {
+  public ID : string;
+  public Nombre : string;
+  public Ubicacion : string;
+  public Descripcion : string;
+  public Telefono : string;
+  public seleccionado: string;
+  public Almacenes: any = [];
 
-  constructor() { }
+  constructor(private APIServices: APIservicesService) { }
 
   ngOnInit(): void {
+    this.GetAlmacen();
+  }
+  GetAlmacenSelected(){
+    for(let almacen of this.Almacenes){
+      if(this.seleccionado == almacen.id){
+        this.ID = almacen.id;
+        this.Nombre = almacen.nombre;
+        this.Descripcion = almacen.descripcion;
+        this.Ubicacion = almacen.ubicacion;
+        this.Telefono = almacen.telefono;
+       
+     
+      }
+    }
+  }
+
+  GetAlmacen(){
+    this.APIServices.GetAlmacen().subscribe((data: {}) => {
+        this.Almacenes = data;
+        console.log(this.Almacenes);
+    })
+    }
+
+   
+
+
+     UpdateAlmacen(newNombre: HTMLInputElement,newDescripcion: HTMLInputElement,newUbicacion: HTMLInputElement,newTelefono: HTMLInputElement) {
+      if(this.seleccionado != null){
+      if(this.seleccionado == this.ID){
+      if (window.confirm('Are you sure, you want to delete?')){
+        const almacen = {
+          Id: this.seleccionado, 
+          Nombre: newNombre.value,
+          Descripcion: newDescripcion.value,
+          Ubicacion: newUbicacion.value,
+          Telefono: newTelefono.value,
+      
+        };
+        this.APIServices.UpdateAlmacen(this.seleccionado, almacen).subscribe(data => {
+          this.GetAlmacen();
+          newNombre.value = '';
+          newDescripcion.value = '';
+          newUbicacion.value = '';
+          newTelefono.value = '';
+          this.seleccionado= "";
+         
+        })
+      }
+      }
+    }  
   }
 
 }
--- src/app/Componentes/Almacenes/eli-almacenes/eli-almacenes.component.ts
+++ src/app/Componentes/Almacenes/eli-almacenes/eli-almacenes.component.ts
@@ -7,11 +7,11 @@ import { APIservicesService } from 'src/app/Servicios/apiservices.service';
   styleUrls: ['./eli-almacenes.component.scss']
 })
 export class EliAlmacenesComponent implements OnInit {
-  public ID : any;
+  public ID : string;
   public Nombre : string;
   public Ubicacion : string;
   public Descripcion : string;
-  public Telefono : number;
+  public Telefono : string;
   public seleccionado: string;
  
 
@@ -26,14 +26,14 @@ export class EliAlmacenesComponent implements OnInit {
 
   GetAlmacenSelected(){
     for(let almacen of this.Almacenes){
-      if(almacen.Id === this.seleccionado){
+      if(this.seleccionado == almacen.id){
         this.ID = almacen.id;
         this.Nombre = almacen.nombre;
         this.Descripcion = almacen.descripcion;
         this.Ubicacion = almacen.ubicacion;
         this.Telefono = almacen.telefono;
-          
-
+       
+     
       }
     }
   }
@@ -49,15 +49,21 @@ export class EliAlmacenesComponent implements OnInit {
 
 
   DeleteAlmacen() {
-    if(this.Almacenes.ID === this.ID){
+    if(this.seleccionado != null){
+    if(this.seleccionado == this.ID){
     if (window.confirm('Are you sure, you want to delete?')){
       this.APIServices.DeleteAlmacen(this.seleccionado).subscribe(data => {
         this.GetAlmacen();
+        this.ID ="";
+        this.Nombre ="";
+        this.Descripcion = "";
+        this.Ubicacion = "";
+        this.Telefono = "";
       })
     }
     }
   }  
-
+}
 
 }
  
