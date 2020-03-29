815cda74b8521e32ca472268dd1bf2273121e271
Erickps123 <erickkpsanchez@gmail.com>
2020-03-28 21:26:31 -0400

    Routing admin

--- src/app/Componentes/navegacion/navegacion.component.ts
+++ src/app/Componentes/navegacion/navegacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { APIservicesService } from 'src/app/Servicios/apiservices.service';
+import {RouterModule, Routes, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navegacion',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navegacion.component.scss']
 })
 export class NavegacionComponent implements OnInit {
-
-  constructor() { }
+  trustedUser= "admin";
+  constructor(private APIServices: APIservicesService, private router: Router) { }
 
   ngOnInit(): void {
+    
+  }
+  Goto(ruta) {
+    this.router.navigate(['/', ruta]);
+  }
+
+  HomeAlmacenes(){
+    if(this.trustedUser !=this.APIServices.getUserLoggedIn()){
+      this.Goto('Home'); 
+    }
+      this.Goto('HomeAlmacenes'); 
+  }
+  HomeSucursal(){
+    if(this.trustedUser !=this.APIServices.getUserLoggedIn()){
+      this.Goto('Home'); 
+    }
+      this.Goto('HomeSucursales'); 
+  }
+  HomeUsuario(){
+    if(this.trustedUser !=this.APIServices.getUserLoggedIn()){
+      this.Goto('Home'); 
+    }
+      this.Goto('HomeUsuarios'); 
   }
 
 }
