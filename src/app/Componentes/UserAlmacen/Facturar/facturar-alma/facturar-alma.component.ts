02c1b3b4be27ee78528866d676b26f9619863008
Hector Sanchez <20186183@itla.edu.do>
2020-03-28 22:38:28 -0400

    View Facturar

--- src/app/Componentes/UserAlmacen/Facturar/facturar-alma/facturar-alma.component.ts
+++ src/app/Componentes/UserAlmacen/Facturar/facturar-alma/facturar-alma.component.ts
@@ -10,16 +10,25 @@ import {RouterModule, Routes, Router } from '@angular/router';
 export class FacturarAlmaComponent implements OnInit {
 
   trustedUser= "Almacen";
+  public seleccionado: string;
+  public Productos: any = [];
   constructor(private APIServices: APIservicesService, private router: Router) { }
 
   ngOnInit(): void {
     if((this.trustedUser !=this.APIServices.getUserLoggedIn())){
       this.Goto('Login'); 
     }
+    this.GetProducto();
   }
 
   Goto(ruta) {
     this.router.navigate(['/', ruta]);
+  }
+  GetProducto(){
+    this.APIServices.GetProducto().subscribe((data: {}) => {
+        this.Productos = data;
+        console.log(this.Productos);
+    })
   }
 
 }
--- src/app/Componentes/UserSucursal/Facturar/facturar/facturar.component.ts
+++ src/app/Componentes/UserSucursal/Facturar/facturar/facturar.component.ts
@@ -10,16 +10,25 @@ import {RouterModule, Routes, Router } from '@angular/router';
 export class FacturarComponent implements OnInit {
 
   trustedUser= "Sucursal";
+  public seleccionado: string;
+  public Productos: any = [];
   constructor(private APIServices: APIservicesService, private router: Router) { }
 
   ngOnInit(): void {
     if((this.trustedUser !=this.APIServices.getUserLoggedIn())){
       this.Goto('Login'); 
     }
+    this.GetProducto();
   }
 
   Goto(ruta) {
     this.router.navigate(['/', ruta]);
+  }
+  GetProducto(){
+    this.APIServices.GetProducto().subscribe((data: {}) => {
+        this.Productos = data;
+        console.log(this.Productos);
+    })
   }
 
 }
